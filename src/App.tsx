6d043eb8cb25a5b70037397e3a7138441c003365
DualDen <disa.shilow2@gmail.com>
2023-09-24 12:47:57 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useSprings, animated, to as interpolate } from '@react-spring/web'
+import { useDrag } from 'react-use-gesture'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from './styles.module.css'
+
+interface ICard {
+  text?: string;
+  image?: string;
 }
 
-export default App;
+const cards: ICard[] = [
+  {
+    text: 'Умной!'
+  },
+    {
+    text: 'Сильной!'
+  },
+    {
+    text: 'Креативной!'
+  },
+    {
+    text: 'Красивой!'
+  },
+  {
+    text: 'Чудесной!'
+  },
+  {
+    text: 'Оставайся всегда такой же :'
+  },
+  {
+    text: 'С днем рождения!'
+  },
+  {
+    image: 'https://sun9-40.userapi.com/impg/EM2C7rE-VqN1eNL8jY_jMvUi_SX9tRZvrpWN0g/qTJQtgy-syQ.jpg?size=736x736&quality=95&sign=509ef40f17dc78f3e99dbb66aadde16b&type=album',
+  }
+]
+
+const to = (i: number) => ({
+  x: 0,
+  y: i * -4,
+  scale: 1.2,
+  rot: -10 + Math.random() * 20,
+  delay: i * 100,
+})
+const from = (_i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
+const trans = (r: number, s: number) =>
+    `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
+
+function Deck() {
+  const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
+  const [props, api] = useSprings(cards.length, i => ({
+    ...to(i),
+    from: from(i),
+  })) // Create a bunch of springs using the helpers above
+  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
+  const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
+    const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
+    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+    api.start(i => {
+      if (index !== i) return // We're only interested in changing spring-data for the current spring
+      const isGone = gone.has(index)
+      const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+      const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
+      const scale = down ? 1.1 : 1 // Active cards lift up a bit
+      return {
+        x,
+        rot,
+        scale,
+        delay: undefined,
+        config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
+      }
+    })
+    if (!down && gone.size === cards.length)
+      setTimeout(() => {
+        gone.clear()
+        api.start(i => to(i))
+      }, 600)
+  })
+  // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+  return (
+      <>
+        {props.map(({ x, y, rot, scale }, i) => (
+            <animated.div className={styles.deck} key={i} style={{ x, y }}>
+              {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+              <animated.div
+                  {...bind(i)}
+                  style={{
+                    transform: interpolate([rot, scale], trans),
+                    backgroundImage: `url(${cards[i]?.image})`,
+                  }}
+              >
+                {cards[i]?.text}
+              </animated.div>
+            </animated.div>
+        ))}
+      </>
+  )
+}
+
+export default function App() {
+  return (
+      <div className={styles.container}>
+        <Deck />
+      </div>
+  )
+}
